70fa3ed9f9194484b6ca726271b8eff46feb4b79
Pandry <17499836+Pandry@users.noreply.github.com>
2023-06-20 18:02:21 +0200

    Improves logging, logged-out check and recursive options

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ if __name__ == '__main__':
     while not ready:
         ready = bridge.is_ready()
         accounts = bridge.list_accounts()
-        if len(accounts) < 1:
+        # We want to login if the account is signed out 
+        non_signed_out_accounts = list(filter(lambda a: a.status != "signed out" ,accounts))
+        if len(non_signed_out_accounts) < 1:
             print("No accounts found. Adding account")
             bridge.add_account(account, password)
             print(f"Added account {account}")
@@ -46,7 +48,7 @@ ______          _             ______            _
     while True:
         for incoming in imap.getUnreadEmailsIter():
 
-            incoming_sender = incoming.get_all('from', [])[0]
+            incoming_sender = email.utils.getaddresses(incoming.get_all('from', []))[0]
             incoming_subject = incoming['Subject']
 
             incoming_tos = incoming.get_all('to', [])
@@ -55,27 +57,33 @@ ______          _             ______            _
             incoming_resent_ccs = incoming.get_all('resent-cc', [])
             incoming_recipients = email.utils.getaddresses(incoming_tos + incoming_ccs + incoming_resent_tos + incoming_resent_ccs)
 
+            incoming_tos = email.utils.getaddresses(incoming_tos)
+
             outgoing_recipients = set()
             outgoing_from = ""
             for recipient_name, recipient_email in incoming_recipients:
                 if recipient_email in incoming_to_intended:
                     outgoing_recipients.update(incoming_to_intended[recipient_email])
                     outgoing_from = recipient_email
+            outgoing_recipients.remove(incoming_sender) # Remove the sender from the set, just in case, to avoid sending a CC to the same person again
+            for addr in incoming_to_intended.keys():
+                outgoing_recipients.remove(addr) # Removes from the set all the aliases, to avoid a loop
             outgoing_recipients = list(outgoing_recipients)
             if len(outgoing_recipients) == 0:
                 imap.readMail(incoming.number)
-                continue 
+                continue
             
             #outgoing = email.message.Message()
             outgoing = incoming
-            #outgoing['From'] =  # outgoing_from # smtplib.SMTPDataError: (554, b'5.0.0 Error: transaction failed, blame it on the weather: invalid return path')
+            # Set the `Sender` to avoid key errors with the rep
+            #outgoing['Sender'] = ""
+            #outgoing.replace_header("Sender", incoming_sender[1])
             outgoing.replace_header("From", account)
             outgoing.replace_header("To", ", ".join(outgoing_recipients))
-            outgoing.replace_header("Subject", f"{incoming_sender} -> {incoming_tos[0]} : {incoming_subject}")
-            outgoing.add_header('X-Original-To', incoming_sender)
-            outgoing.add_header('Delivered-To', incoming_sender)
+            outgoing.replace_header("Subject", f"{incoming_sender} -> {incoming_tos[0][1]}: {incoming_subject}")
+            #outgoing.add_header('Reply-To', incoming_sender[1])
 
-            print(f"Got mail \"{incoming_subject}\" from {incoming_sender[0]} ({incoming_sender[1]}); sending to {outgoing_recipients}")
+            print(f"Got mail \"{incoming_subject}\" from \"{incoming_sender[0]}\" ({incoming_sender[1]}); sending to {outgoing_recipients}")
             smtp.sendMessage(outgoing)
             # Set read flag so we don't reprocess the message
             imap.readMail(incoming.number)
